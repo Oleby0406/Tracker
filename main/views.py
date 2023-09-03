b20381545bba1dd156186e3d78884f0ddd30d24d
Oleby0406 <oleg.bychenkov@gmail.com>
2023-09-03 14:40:23 -0700

    Fix file paths

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
-sharedVars = open("main/sharedVars.txt", "r")
-content = sharedVars.readlines()
 
 def main(request):
+    file_path = os.path.join(os.path.dirname(__file__), 'sharedVars.txt')
+    sharedVars = open(file_path, "r")
+    content = sharedVars.readlines()
     return render(request, 'main.html', {'times': content[0][:-1].strip('][').split(','),
                                          'averages': content[1][:-1].strip('][').split(','),
                                          'min': content[2], 
